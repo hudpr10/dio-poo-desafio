742ff9b61af1a3a3dc27a8a3850524003389adc2
Hudson Reis <hudsonpr09@gmail.com>
2024-09-05 16:54:53 -0300

    Finalizando os TODO e realizando testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("5535931845093", "Nokia", "038210983109", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone = new Iphone("5535980716451", "iPhone", "039210983109", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");  
